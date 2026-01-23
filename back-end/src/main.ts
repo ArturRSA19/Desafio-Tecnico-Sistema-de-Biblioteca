4ab3eeea6506720880c27ff63d7b4cc0cc87a00c
ArturRSA19 <artur190503@outlook.com>
2026-01-23 15:04:55 -0300

    feat: habilitando CORS para requisições

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Habilita CORS para permitir requisições do frontend
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  
   // Habilita validação global com class-validator
   app.useGlobalPipes(
     new ValidationPipe({
